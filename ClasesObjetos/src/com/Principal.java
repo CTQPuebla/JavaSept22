2dc08f674a102b67c0a45c9eec7161bce47774ad
CTQPuebla <pruebasceteq@hotmail.com>
2022-09-27 11:12:11 -0500

    practica de la navaja

---
+++ ClasesObjetos/src/com/Principal.java
@@ -0,0 +1,32 @@
+package com;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		//Crear el objeto
+		
+		Tablet t1 = new Tablet();		
+		System.out.println(t1);
+		
+		t1.setMarca("Lenovo");
+		t1.setColor("Negro");
+		t1.setLargo(12.5);
+		t1.setMateriales("Metal");
+		t1.setBotones(1);
+		t1.setAncho(8);
+		
+		System.out.println(t1);
+		
+		Tablet t2 = new Tablet("Plata", "Asus", "Aluminio", 10.5, 5.1, 3);
+		System.out.println(t2);
+		//Objetos que forman parte de Tablet		
+		Camara c1 = new Camara(20, 5, 2.6);
+		Almacenamiento a1 = new Almacenamiento("Qualcom", 32);
+		
+		Tablet t3 = new Tablet("Azul", "Samsung", "Plastico", 15, 7, 3, c1, a1);
+		System.out.println(t3);
+		
+	}
+
+}
